304d026ead5bcd56b24d119baf0fb821b4b666fb
Timothy <timmyva@users.noreply.github.com>
2018-03-14 13:02:11 -0700

    3/14/18

--- Source/DuckHunt/DuckHuntProjectile.cpp
+++ Source/DuckHunt/DuckHuntProjectile.cpp
@@ -28,7 +28,7 @@ ADuckHuntProjectile::ADuckHuntProjectile()
 	ProjectileMovement->bShouldBounce = true;
 
 	// Die after 3 seconds by default
-	InitialLifeSpan = 3.0f;
+	InitialLifeSpan = 2.0f;
 }
 
 void ADuckHuntProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
